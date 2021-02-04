fa1631f406581d3f66c3d19e76a1310ea8c759f6
leigh90 <zawadikibathi@gmail.com>
2021-02-04 08:13:19 +0300

    Add book componenent

---
+++ src/Book/Book.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const book = (props) => {
+    return (
+        <div>
+            <p>Book Component: I am a book </p>
+            <p></p>
+        </div>
+    )
+    
+}
+export default book
